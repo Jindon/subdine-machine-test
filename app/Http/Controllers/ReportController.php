edde6933b677bfbf7ca37ff7d0aedfdf3cd50fd1
Jindon Moirangthem <jindon27@gmail.com>
2021-01-16 15:26:13 +0530

    add report for most and least sale

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -17,10 +17,12 @@ class ReportController extends Controller
         $dishes =  Dish::addSelect([
                     'quantity' => Order::selectRaw('sum(quantity) as total')
                                     ->whereColumn('dish_id', 'dishes.id')
-                                    ->where('created_at', '>=', $date),
+                                    ->where('created_at', '>=', $date)
+                                    ->groupBy('dish_id'),
                     'sale_amount' => Order::selectRaw('sum(amount) as total')
                                         ->whereColumn('dish_id', 'dishes.id')
-                                        ->where('created_at', '>=', $date),
+                                        ->where('created_at', '>=', $date)
+                                        ->groupBy('dish_id'),
                     ])
                     ->withCount(['orders' => function($query) use($date) {
                         $query->where('created_at', '>=', $date);
@@ -32,6 +34,34 @@ class ReportController extends Controller
 
     public function saleOverview()
     {
+        $date = Carbon::today()->subDays(10);
 
+        $mostSold = $this->mostLeastSaleQuery($date, 'desc', 5);
+
+        $leastSold = $this->mostLeastSaleQuery($date, 'asc', 5);
+
+
+        return response()->json([
+            'data' => [
+                'most_sold' => $mostSold,
+                'least_sold' => $leastSold,
+            ]
+        ]);
+
+    }
+
+    protected function mostLeastSaleQuery($date, $order, $take)
+    {
+        return Dish::addSelect([
+            'quantity' => Order::selectRaw('sum(quantity) as total')
+                ->whereColumn('dish_id', 'dishes.id')
+                ->where('created_at', '>=', $date)
+                ->groupBy('dish_id'),
+
+            'sale_amount' => Order::selectRaw('sum(amount) as total')
+                ->whereColumn('dish_id', 'dishes.id')
+                ->where('created_at', '>=', $date)
+                ->groupBy('dish_id'),
+            ])->orderBy('quantity', $order)->take($take)->get();
     }
 }
